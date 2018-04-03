711d3da8fa3c17c2ec7f129f72c4f5d82441b0e4
guba1a <1514850@my.brunel.ac.uk>
2018-04-03 22:00:06 +0100

    Navigation drawer and number of blank fragments added.

---
+++ app/src/main/java/com/example/guba1a/londonnon_league/HITA.java
@@ -0,0 +1,29 @@
+package com.example.guba1a.londonnon_league;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HITA extends Fragment {
+
+
+    public HITA() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hita, container, false);
+    }
+
+}
