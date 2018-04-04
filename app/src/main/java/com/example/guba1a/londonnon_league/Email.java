b54b4bd413db87390b2e6d1cc911eeeb0b75f02e
guba1a <1514850@my.brunel.ac.uk>
2018-04-04 02:25:09 +0100

    Email fragment updated.

--- app/src/main/java/com/example/guba1a/londonnon_league/Email.java
+++ app/src/main/java/com/example/guba1a/londonnon_league/Email.java
@@ -1,17 +1,40 @@
 package com.example.guba1a.londonnon_league;
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.example.guba1a.londonnon_league.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Email extends Fragment {
+
+    @BindView(R.id.contact_name_wrapper)
+    TextInputLayout emailNameWrapper;
+    @BindView(R.id.contact_name)
+    EditText emailName;
+
+    @BindView(R.id.contact_subject_wrapper)
+    TextInputLayout emailSubjectWrapper;
+    @BindView(R.id.contact_subject)
+    EditText emailSubject;
+
+    @BindView(R.id.contact_message_wrapper)
+    TextInputLayout emailMessageWrapper;
+    @BindView(R.id.contact_message)
+    EditText emailMessage;
+
+    @BindView(R.id.send_message)
+    Button sendEmail;
 
 
     public Email() {
@@ -22,8 +45,56 @@ public class Email extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_email, container, false);
+        ButterKnife.bind(this, view);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email, container, false);
+        return view;
+    }
+
+    @OnClick(R.id.send_message)
+    public void buttonPostMessage(){
+
+        String name = emailName.getText().toString();
+        String subject = emailSubject.getText().toString();
+        String message = emailMessage.getText().toString();
+
+        if (TextUtils.isEmpty(name)){
+            emailNameWrapper.setError("Enter Your Name");
+            emailName.requestFocus();
+            return;
+        } else {
+            emailNameWrapper.setErrorEnabled(false);
+        }
+
+        if (TextUtils.isEmpty(subject)){
+            emailSubjectWrapper.setError("Enter Your Subject");
+            emailSubject.requestFocus();
+            return;
+        } else {
+            emailSubjectWrapper.setErrorEnabled(false);
+        }
+
+        if (TextUtils.isEmpty(message)){
+            emailMessageWrapper.setError("Enter Your Message");
+            emailMessage.requestFocus();
+            return;
+        } else {
+            emailMessageWrapper.setErrorEnabled(false);
+        }
+
+        Intent sendEmail = new Intent(android.content.Intent.ACTION_SEND);
+
+        // Fill it with Data
+        sendEmail.setType("plain/text");
+        sendEmail.putExtra(android.content.Intent.EXTRA_EMAIL,
+                new String[]{"1514850@my.brunel.ac.uk"});
+        sendEmail.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        sendEmail.putExtra(android.content.Intent.EXTRA_TEXT,
+                "Name: " + name + "\nMessage:\n" + message);
+
+        // Send it off to the activity chooser
+        startActivity(Intent.createChooser(sendEmail, "Send Email"));
+
     }
 
 }
--- app/src/main/java/com/example/guba1a/londonnon_league/MainActivity.java
+++ app/src/main/java/com/example/guba1a/londonnon_league/MainActivity.java
@@ -21,10 +21,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,13 +33,13 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         // When the application starts, the Location fragment will be displayed
         setTitle("Location ");
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -133,7 +133,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
